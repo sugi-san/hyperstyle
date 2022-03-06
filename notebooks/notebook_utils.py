7504eaf78dea58acd0866e2af4899a92f86a0774
Yuichi Sugita <34782651+cedro3@users.noreply.github.com>
2022-03-06 13:24:27 +0900

    Update notebook_utils.py

--- notebooks/notebook_utils.py
+++ notebooks/notebook_utils.py
@@ -57,10 +57,10 @@ def run_alignment(image_path):
     import dlib
     from scripts.align_faces_parallel import align_face
     if not os.path.exists("shape_predictor_68_face_landmarks.dat"):
-        print('Downloading files for aligning face image...')
+        #print('Downloading files for aligning face image...')
         os.system('wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2')
         os.system('bzip2 -dk shape_predictor_68_face_landmarks.dat.bz2')
-        print('Done.')
+        #print('Done.')
     predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
     aligned_image = align_face(filepath=image_path, predictor=predictor)
     #print(f"Finished running alignment on image: {image_path}")
